c58cb91a695608733efee410c23774ffb9cc9cd7
gitAasharAzeem <aaashar22@gmail.com>
2024-10-03 22:37:00 +0500

    fixes

--- src/components/products/ProductSmartMonitorSection.tsx
+++ src/components/products/ProductSmartMonitorSection.tsx
@@ -2,11 +2,11 @@ import styles from '../css/products/ProductSmartMonitorSection.module.css';
 
 const ProductSmartMonitorSection = () => {
     return (
-        <section className="container">
+        <section className="container-fluid">
             <div className="row">
                 <div className={`col-md-7 ${styles.smartMonitorLeft}`}>
                     <h1 className={styles.smartMonitorTitle}>AI Monitoring System</h1>
-                    <div className="w-75">
+                    <div className="">
                         <p className={styles.smartMonitorText}>
                             <b>Stop, Shield & Sustainability </b><br />
                             Let AI to detect and stop leaks at the source. It provides real-time alerts when leaks occur and can automatically shut off the water supply. Through intelligent monitoring, it identifies leak sources and waste, mitigating potential damage while reducing water consumption and cutting carbon emissions.
