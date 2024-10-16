dd73f86bff5b3fcb515f5264dae7251357c288f1
gitAasharAzeem <aaashar22@gmail.com>
2024-10-17 02:11:35 +0500

    add component and animation

--- src/components/products/aimonitoringsystem/ScrollQuestions.tsx
+++ src/components/products/aimonitoringsystem/ScrollQuestions.tsx
@@ -43,7 +43,7 @@ const ScrollQuestions = () => {
                     setIsQuestionSectionActive(false); // Allow scrolling after last question
                 }
                 setIsAnimating(false);
-            }, 1000); // Adjust time between question changes
+            }, 300); // Adjust time between question changes
         }
 
         // Scroll up to show the previous question
@@ -57,7 +57,7 @@ const ScrollQuestions = () => {
                     setIsQuestionSectionActive(false); // Allow scrolling after reaching the first question
                 }
                 setIsAnimating(false);
-            }, 1000);
+            }, 300);
         }
     }, [isAnimating, isQuestionSectionActive, currentQuestionIndex, questions.length]);
 
